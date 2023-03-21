07330e733bf07b2405c6f60d18aa91591ef476b9
Paulo Rupp <paulo.rupp198@gmail.com>
2023-03-21 13:21:41 -0300

    add some testing

--- test/tes_name_generator_test.exs
+++ test/tes_name_generator_test.exs
@@ -2,7 +2,15 @@ defmodule TesNameGeneratorTest do
   use ExUnit.Case
   doctest TesNameGenerator
 
-  test "greets the world" do
-    assert TesNameGenerator.hello() == :world
+  test "tests all races and genders" do
+    data = TesNameGenerator.Impl.NameList.names_data()
+    race_list = ["altmer", "argonian", "bosmer", "breton", "dunmer", "imperial", "khajiit", "nord"]
+
+    Enum.each(race_list, fn
+      race ->
+        function = String.to_atom("get_" <> race)
+        apply(TesNameGenerator.Impl.NameList, function, [data, :male])
+        apply(TesNameGenerator.Impl.NameList, function, [data, :female])
+    end)
   end
 end
